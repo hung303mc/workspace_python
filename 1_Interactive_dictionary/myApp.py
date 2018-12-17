eeda99846f8db10e413f7b46c2698d67c487b34c
hung303mc <hung216mc@gmail.com>
2018-12-17 16:50:54 +0700

    Refactor code for interactive dict

--- 1_Interactive_dictionary/myApp.py
+++ 1_Interactive_dictionary/myApp.py
@@ -13,15 +13,21 @@ def translate(word):
     tmp = get_close_matches(word, data.keys(), 1, 0.8)
 
   if len(tmp) > 0:
-    print("did you mean %s instead? " % tmp[0])
-    ans = input("Enter Y if yes, or N if no: ")
+    ans = input("did you mean %s instead?Enter Y if yes, or N if no:" % tmp[0])
     if ans == "Y":
       return data[tmp[0]]
     elif ans == "N":
-      return ("couldn't find word " + word)
+      return "couldn't find word "
+    else:
+      return "we couldn't know what you want"
   else:
-    return ("couldn't find word " + word)
+    return "couldn't find word "
 
 word = input("Enter word: ")
 
-print(translate(word))
+output = translate(word)
+if type(output) == list:
+  for item in output:
+    print(item)
+else:
+  print(output)
